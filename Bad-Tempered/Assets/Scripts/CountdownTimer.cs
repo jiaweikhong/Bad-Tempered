2edfce90726c29f9cbac5be74df929cdfe0f2da8
Khong Jia Wei <khongjw@gmail.com>
2020-02-01 13:29:38 +0800

    major edits

---
+++ Bad-Tempered/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float myTimer = 120;
+    public Text timerText;
+    private bool timerIsActive = true;
+
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Update()
+    {
+        if (timerIsActive)
+        {
+            myTimer -= Time.deltaTime;
+            timerText.text = myTimer.ToString("f0");
+            print(myTimer);
+
+            if (myTimer <= 0)
+            {
+                myTimer = 0;
+                timerIsActive = false;
+                SceneManager.LoadScene("lose");
+
+            }
+        }
+
+    }
+}
