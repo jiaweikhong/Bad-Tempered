0d85db69609369172292db854f3c60368f76ca4d
Khong Jia Wei <khongjw@gmail.com>
2020-02-01 23:12:24 +0800

    included wipes cursor

---
+++ Bad-Tempered/Assets/Scripts/CursorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    //public GameObject cursor;
+    private Vector2 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = mousePos;
+    }
+
+    /*private void OnMouseDown()
+    {
+        Debug.Log("hey");
+    }*/
+}
