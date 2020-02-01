95e6de2dc3d3e70fdda59050d8554cdaef38fbc4
Khong Jia Wei <khongjw@gmail.com>
2020-02-01 21:05:19 +0800

    merged well

---
+++ Bad-Tempered/Assets/Scripts/PasteButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class PasteButton : MonoBehaviour
+{
+    private int finalScore;
+    float timeMouseDown = 0.0f;
+    public static bool mouseIsDown;
+    public ScreenProtector screenProtector;
+    public Sprite PressedButtonImg;
+    public Sprite NormalButtonImg;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = NormalButtonImg;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseIsDown == true)
+        {
+            Debug.Log("MouseDown!");
+            timeMouseDown += Time.deltaTime;
+            spriteRenderer.sprite = PressedButtonImg;
+            if (timeMouseDown > 1.0f)
+            {
+                screenProtector.isPastedOn = true;
+                finalScore = Mathf.RoundToInt(ScreenProtector.score);
+                PointsScript.points += finalScore;
+                mouseIsDown = false;
+                SceneManager.LoadScene("CheckStage");
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            timeMouseDown = 0.0f;
+            spriteRenderer.sprite = NormalButtonImg;
+
+        }
+    }
+
+
+    private void OnMouseDown()
+    {
+        mouseIsDown = true;
+    }
+
+    private void OnMouseUp()
+    {
+        mouseIsDown = false;
+    }
+
+}
---
+++ Bad-Tempered/Assets/Scripts/ScreenProtector.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScreenProtector : MonoBehaviour
+{
+    float speed = 1.0f;
+    float boundX = 6.0f;
+    float boundY = 10.0f;
+    //float timeBenchmark = 3.0f;
+    //float timeElapsed = 0.0f;
+    float maxScore = 60.0f;
+    public static float score = 0.0f;
+    public bool isPastedOn = false;
+    SpriteRenderer m_SpriteRenderer;
+    float yellowRange = 1.0f;
+    float greenRange = 0.1f;
+    float deductScoreMultiplier = 3.0f;
+    public SpriteRenderer backing;
+
+    //If stage completed within 3 secs, no airbubbles
+    //Else for every sec elapsed after 3 secs,+1 airbubble for next stage.
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //Timer for 3 seconds start!
+        transform.position = new Vector3(1.0f, 1.0f, 0);
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SpriteRenderer.color = Color.magenta;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 newPos = Vector3.zero;
+        //Use coroutines to segment parts of the game
+        //1st coroutine is the alignment of the screen protector. If position hit, go to next coroutine
+        //2nd coroutine ensures alignment of screenprotector is at (0,0) for 1 sec. If completed, prompt to go next stage appears?(tap enabled) Else,go back to 1st coroutine
+        //Calculate and bring over the number of air bubbles to be generated in the next stage.
+        float inputX = Input.acceleration.x * speed;
+        float inputY = Input.acceleration.y * speed;
+        Vector3 tempPos = new Vector3(transform.position.x + inputX, transform.position.y + inputY);
+
+        // Prevent screen protector from going out of bounds
+        if (tempPos.x > boundX || tempPos.x < -boundX || tempPos.y > boundY || tempPos.y < -boundY)
+        {
+            //Do Nothing
+        }
+        else
+        {
+            transform.position = tempPos;
+        }
+
+        // Check how accurate user places the screen protector.
+        
+        if (transform.position.x < greenRange && transform.position.x > -greenRange && transform.position.y < greenRange && transform.position.y > -greenRange)
+        {
+            m_SpriteRenderer.color = Color.green;
+        } else if (transform.position.x < yellowRange && transform.position.x > -yellowRange && transform.position.y < yellowRange && transform.position.y > -yellowRange)
+        {
+            m_SpriteRenderer.color = Color.yellow;
+        } else
+        {
+            m_SpriteRenderer.color = Color.magenta;
+        }
+        score = maxScore - (Mathf.Abs(transform.position.x) * deductScoreMultiplier) - Mathf.Abs(transform.position.y);
+    }
+
+}
